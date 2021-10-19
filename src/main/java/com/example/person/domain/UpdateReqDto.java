92431a0d07d33e0f0115f826345e6d478765f116
wooseokoh <allsafe1206@gmail.com>
2021-10-19 23:32:19 +0900

    aop start

---
+++ src/main/java/com/example/person/domain/UpdateReqDto.java
@@ -0,0 +1,12 @@
+package com.example.person.domain;
+
+import javax.validation.constraints.NotBlank;
+
+import lombok.Data;
+
+@Data
+public class UpdateReqDto {
+	@NotBlank(message = "password를 입력하지 않았습니다.")
+	private String password;
+	private String phone;
+}
